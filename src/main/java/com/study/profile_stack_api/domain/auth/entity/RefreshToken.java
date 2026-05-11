29ea76170639c15143425d4828c0cdee6e59547f
siho <dolphin@kakao.com>
2026-05-11 22:42:48 +0900

    chore: entity에 jpa 어노테이션 추가

--- src/main/java/com/study/profile_stack_api/domain/auth/entity/RefreshToken.java
+++ src/main/java/com/study/profile_stack_api/domain/auth/entity/RefreshToken.java
@@ -1,5 +1,6 @@
 package com.study.profile_stack_api.domain.auth.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,14 +8,26 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "refresh_token")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class RefreshToken {
-    private Long id;                    // id
-    private Long memberId;              // memberId
-    private String token;               // 토큰
-    private LocalDateTime expiredDate;    // 만료일
-    private LocalDateTime createdAt;    // 생성일
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "member_id", nullable = false, unique = true)
+    private Long memberId;
+
+    @Column(nullable = false, length = 500)
+    private String token;
+
+    @Column(name = "expiry_date", nullable = false)
+    private LocalDateTime expiredDate;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 }
